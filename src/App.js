eaf7babd373649006d0e1456efc8cb9fefeb83c9
A Selassie <atsede@gmail.com>
2019-08-07 22:28:02 +0100

    steps 1-3 done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import amelie from './amelie.jpg';
+import nemo from './nemo.jpg';
+import takeball from './takeball.jpg';
+import toystory from './toystory.jpeg';
+import tsmm from './tsmm.jpg';
+import talk from './hable_con_ella.jpg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <h1>Welcome to Kodflix!!!</h1>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+          {/* <h1>Welcome to Kodflix</h1> */}
+           <div className='container'>
+            <img src={amelie} alt='amelie poster'/>
+            <img src={nemo} alt='finding nemo poster'/>
+            <img src={takeball} alt='take the ball poster'/>
+            <img src={toystory} alt='toy story poster'/>
+            <img src={tsmm} alt='all about poster'/>
+            <img src={talk} alt='talk to her poster'/>
+           </div>
+           <div className='titles'>
+            <h1>Amelie</h1>
+            <h1>Finding Nemo</h1>
+            <h1>Take The Ball Pass The Ball</h1>
+            <h1>Toy Story 4</h1>
+            <h1>Todo Sobre Mi Madre</h1>
+            <h1>Hable Con Ella</h1>
+           </div>
+          
     </div>
   );
 }
