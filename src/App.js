7b64cba6b91f2ddbbdb3c30377bd7138087ea0b6
A Selassie <atsede@gmail.com>
2019-08-11 11:06:23 +0100

    task 5  - show titles on images on hover

--- src/App.js
+++ src/App.js
@@ -14,36 +14,33 @@ function App() {
       <div className="container">
         <div className="item">
           <img src={fifty} alt="50/50 poster" />
+          <div className="element" id='first'>50/50</div>
         </div>
         <div className="item">
           <img src={nemo} alt="finding nemo poster" />
+          <div className="element" id="second">Finding Nemo</div> 
         </div>
         <div className="item">
           <img src={takeball} alt="take the ball poster" />
+          <div className="element" id="third">Take The Ball Pass The Ball</div>
         </div>
       </div>
       <div className="container">
         <div className="item">
           <img src={toystory} alt="toy story poster" />
+          <div className="element" id="fourth">Toy Story 4</div>
         </div>
         <div className="item">
           <img src={tsmm} alt="all about poster" />
+          <div className="element" id="fifth">Todo Sobre Mi Madre</div>
         </div>
         <div className="item">
           <img src={talk} alt="talk to her poster" />
+          <div className="element" id="sixth">Hable Con Ella</div>
         </div>
       </div>
 
-      <div className="wrapper">
-        <div className="element">Amelie</div>
-        <div className="element">Finding Nemo</div>
-        <div className="element">Take The Ball Pass The Ball</div>
-      </div>
-      <div className="wrapper">
-        <div className="element">Toy Story 4</div>
-        <div className="element">Todo Sobre Mi Madre</div>
-        <div className="element">Hable Con Ella</div>
-      </div>
+      
     </div>
   );
 }
